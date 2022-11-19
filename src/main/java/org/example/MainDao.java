eb2fb32dc3fe55a65e69d9e8de443d0e4e0ae33c
damiansolkowicz <damiansolkowicz@outlook.com>
2022-11-19 11:18:01 +0100

    read

--- src/main/java/org/example/MainDao.java
+++ src/main/java/org/example/MainDao.java
@@ -4,10 +4,6 @@ public class MainDao {
 
     public static void main(String[] args) {
         UserDao userDao= new UserDao();
-        User user1=new User();
-        user1.setUserName("Jan");
-        user1.setEmail("JanKowalski@onet.pl");
-        user1.setPassword("Jankowlaksi");
-        userDao.create(user1);
+        System.out.println(userDao.read(7));
     }
 }
--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -3,11 +3,12 @@ package org.example;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class UserDao {
-    private static final String CREATE_USER_QUERY =
-            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-
+    private static final String CREATE_USER_QUERY = "INSERT INTO worksop2.users(username, email, password) VALUES (?, ?, ?)";
+    private static final String DELETE_QUERY = "DELETE FROM wrokshop2.users where id = ?";
+    private static final String READ_QUERY="SELECT * FROM  users where id=?";
 
 
     public String hashPassword(String password) {
@@ -33,5 +34,43 @@ public class UserDao {
             return null;
         }
     }
+    public User read(int userId) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_QUERY);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserName(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-}
+    public void update(User user) {
+
+    }
+    public void delete(int userId) {
+        try (Connection conn=DbUtil.getConnection(); PreparedStatement statement =
+                         conn.prepareStatement(DELETE_QUERY);) {
+                statement.setInt(1, userId);
+                statement.executeUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+    }
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
+        tmpUsers[users.length] = u;
+        return tmpUsers;
+    }
+
+    }
+
